6c420d2c84cbf25a23c076d6ed533131c5367cd5
Krishnan SR <>
2024-02-06 21:50:55 +0530

    Initial POC for Event Manager

---
+++ app/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.get('/ping', (req, res) => {
+    res.send({
+        status: 'ok'
+    })
+})
+
+module.exports = app
